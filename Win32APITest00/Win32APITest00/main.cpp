98267e8a80beca5db3c0d0bd1ccb1cf24a428740
exwnest <exwnest@gmail.com>
2015-10-24 21:15:02 +0900

    Add Buttons

--- Win32APITest00/Win32APITest00/main.cpp
+++ Win32APITest00/Win32APITest00/main.cpp
@@ -8,6 +8,17 @@ int CALLBACK wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmd
 	ATOM mainWindowClass = RegisterClassExW(&mainWindow);
 	HWND hMainWindow = CreateWindowExW(WS_EX_OVERLAPPEDWINDOW, (LPCWSTR)mainWindowClass, L"Window", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
 	ShowWindow(hMainWindow, nCmdShow);
+	HWND hButton = CreateWindowExW(0, L"BUTTON", L"Button", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 0, 0, 96, 24, hMainWindow, NULL, hInstance, NULL);
+	HWND hButton2 = CreateWindowExW(0, L"BUTTON", L"Button", WS_CHILD | WS_VISIBLE | BS_AUTOCHECKBOX, 0, 24, 96, 24, hMainWindow, NULL, hInstance, NULL);
+	HWND hButton3 = CreateWindowExW(0, L"BUTTON", L"Button", WS_CHILD | WS_VISIBLE | BS_AUTO3STATE, 0, 24 * 2, 96, 24, hMainWindow, NULL, hInstance, NULL);
+	HWND hButton4 = CreateWindowExW(0, L"BUTTON", L"Button", WS_CHILD | WS_VISIBLE | BS_GROUPBOX, 0, 24 * 3, 96, 24, hMainWindow, NULL, hInstance, NULL);
+	HWND hButton5 = CreateWindowExW(0, L"BUTTON", L"Button", WS_CHILD | WS_VISIBLE | WS_GROUP | BS_AUTORADIOBUTTON, 0, 24 * 4, 96, 24, hMainWindow, NULL, hInstance, NULL);
+	HWND hButton6 = CreateWindowExW(0, L"BUTTON", L"Button", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 0, 24 * 5, 96, 24, hMainWindow, NULL, hInstance, NULL);
+	HWND hButton7 = CreateWindowExW(0, L"BUTTON", L"Button", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 0, 24 * 6, 96, 24, hMainWindow, NULL, hInstance, NULL);
+	HWND hButton8 = CreateWindowExW(0, L"BUTTON", L"Button", WS_CHILD | WS_VISIBLE | WS_GROUP | BS_AUTORADIOBUTTON, 0, 24 * 7, 96, 24, hMainWindow, NULL, hInstance, NULL);
+	HWND hButton9 = CreateWindowExW(0, L"BUTTON", L"Button", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 0, 24 * 8, 96, 24, hMainWindow, NULL, hInstance, NULL);
+	HWND hButton10 = CreateWindowExW(0, L"BUTTON", L"Button", WS_CHILD | WS_VISIBLE | BS_AUTORADIOBUTTON, 0, 24 * 9, 96, 24, hMainWindow, NULL, hInstance, NULL);
+	HWND hButton11 = CreateWindowExW(0, L"BUTTON", L"Button", WS_CHILD | WS_VISIBLE | BS_PUSHBOX, 0, 24 * 10, 96, 24, hMainWindow, NULL, hInstance, NULL);
 	MSG msg;
 	BOOL bRet;
 	while ((bRet = GetMessageW(&msg, hMainWindow, 0, 0)) != 0)
